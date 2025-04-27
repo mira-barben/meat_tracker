7192ab82da59fc607d64a30f4affec7addf96fd4
mira-barben <105478860+mira-barben@users.noreply.github.com>
2025-04-27 15:12:37 +0200

    Update meat_tracker.py

--- meat_tracker.py
+++ meat_tracker.py
@@ -134,7 +134,7 @@ if username:
             <div style='background-color:#d4edda;padding:20px;border-radius:10px;border-left:5px solid green;'>
                 <strong>🌿 You’ve completed {full_weeks} full meat-free week{'s' if full_weeks > 1 else ''}!</strong><br>
                 💚🐄🐖🐥🐏🐟💚<br>
-                🌱Keep it up!
+                🌿Keep it up!
             </div>
             """, unsafe_allow_html=True)
 
